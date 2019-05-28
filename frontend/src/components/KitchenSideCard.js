5fa5f25346c52efc78adb39eeee092637eb2da1d
jyan95 <jyan0602@gmail.com>
2019-05-28 15:19:31 -0400

    fixing kitchenshowslide

--- frontend/src/components/KitchenSideCard.js
+++ frontend/src/components/KitchenSideCard.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Card } from 'semantic-ui-react';
 
 const KitchenSideCard = props => {
-  const { id, name, dishes } = props.kitchen;
+  const { id, name, recipes } = props.kitchen;
   return (
     <Card
       onClick={() => props.showDetails(id)}
       header={name}
+      meta={recipes.length}
     />
   )
 }
--- frontend/src/containers/KitchenShowSide.js
+++ frontend/src/containers/KitchenShowSide.js
@@ -5,19 +5,9 @@ class KitchenShowSide extends React.Component {
   render(){
     const { id, name, recipes } = this.props.kitchen;
     return(
-      <Item.Group>
-        {recipes.map(r => ({
-          <Item>
-            <Item.Content>
-              <Item.Header as='a'>{r.name}</Item.Header>
-              <Item.Meta>Ingredients</Item.Meta>
-              <Item.Description>
-                {r.recipe_ingredients.map(ri => ri.quantity)}
-              <Item.Description/>
-            </Item.Content>
-          </Item>
-        }))}
-      </Item.Group>
+      <div>
+
+      </div>
     )
   }
 }
