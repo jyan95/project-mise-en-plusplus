942c006af7edd1f8910752f47629a547e80b09f3
jyan95 <jyan0602@gmail.com>
2019-05-29 14:17:50 -0400

    feature working

--- frontend/src/CookingPage.js
+++ frontend/src/CookingPage.js
@@ -3,10 +3,98 @@ import Navbar from './containers/Navbar';
 import RecipeCooking from './components/RecipeCooking';
 import { Grid, Segment } from 'semantic-ui-react';
 
+let kitchen = {
+  id: 1,
+  name: "French Southern Territories",
+  recipes: [
+    {
+      id: 1,
+      name: "French Fries with Sausages",
+      ingredients: [
+        {
+          id: 93,
+          ingredient: "Blue Eye Trevalla",
+          quantity: "1/4 teaspoon"
+        },
+        {
+          id: 149,
+          ingredient: "Spearmint",
+          quantity: "3 gallon"
+        },
+        {
+          id: 181,
+          ingredient: "Plums",
+          quantity: "1/3 teaspoon"
+        },
+        {
+          id: 182,
+          ingredient: "Garlic",
+          quantity: "2 gallon"
+        }
+      ],
+      instructions: [
+        {
+          id: 19,
+          recipe_id: 1,
+          stage: 1,
+          action: "agree",
+          duration: 113,
+          created_at: "2019-05-29T13:34:42.145Z",
+          updated_at: "2019-05-29T13:34:42.145Z"
+        },
+        {
+          id: 81,
+          recipe_id: 1,
+          stage: 1,
+          action: "omit",
+          duration: 285,
+          created_at: "2019-05-29T13:34:42.342Z",
+          updated_at: "2019-05-29T13:34:42.342Z"
+        },
+        {
+          id: 97,
+          recipe_id: 1,
+          stage: 1,
+          action: "humiliate",
+          duration: 155,
+          created_at: "2019-05-29T13:34:42.405Z",
+          updated_at: "2019-05-29T13:34:42.405Z"
+        },
+        {
+          id: 123,
+          recipe_id: 1,
+          stage: 1,
+          action: "sow",
+          duration: 84,
+          created_at: "2019-05-29T13:34:42.501Z",
+          updated_at: "2019-05-29T13:34:42.501Z"
+        }
+      ]
+    }
+  ]
+};
+
 class CookingPage extends React.Component{
+  constructor(props){
+    super(props);
+    let { id, name, recipes } = kitchen; //this.props.kitchen
+    this.state = {
+      todos: recipes.filter(r => (r.instructions.some(i => (i.stage === 1)))),
+      inProgress:[],
+      finished:[]
+    }
+  }
+
+  update = () => {
+    this.setState({
+      todos: kitchen.recipes.filter(r => (r.instructions.some(i => (i.stage === 1)))),
+      inProgress: kitchen.recipes.filter(r => (r.instructions.some(i => (i.stage === 2)))),
+      finished: kitchen.recipes.filter(r => (r.instructions.some(i => (i.stage === 3))))
+    })
+  }
 
   render(){
-    let kitchen = this.props.kitchen;
+    // let kitchen = this.props.kitchen; pulled from kitchen show
 
     return(
       <React.Fragment>
@@ -18,25 +106,19 @@ class CookingPage extends React.Component{
               </Grid.Column>
               <Grid.Column width={4}>
                 To Do
-                {kitchen.recipes.map(r => ({
-                  <RecipeCooking recipe={r}/>
-                }))}
+                {this.state.todos.map(r => <RecipeCooking recipe={r} update={this.update} stage={1}/>)}
               </Grid.Column>
               <Grid.Column width={1}>
               </Grid.Column>
               <Grid.Column width={4}>
                 In Progress
-                {kitchen.recipes.map(r => ({
-                  <RecipeCooking recipe={r}/>
-                }))}
+                {this.state.inProgress.map(r => <RecipeCooking recipe={r} update={this.update} stage={2}/>)}
               </Grid.Column>
               <Grid.Column width={1}>
               </Grid.Column>
               <Grid.Column width={4}>
                 Finished
-                {kitchen.recipes.map(r => ({
-                  <RecipeCooking recipe={r}/>
-                }))}
+                {this.state.finished.map(r => <RecipeCooking recipe={r} update={this.update} stage={3}/>)}
               </Grid.Column>
               <Grid.Column width={1}>
               </Grid.Column>
--- frontend/src/components/RecipeCooking.js
+++ frontend/src/components/RecipeCooking.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+import { Container, Header, Card } from 'semantic-ui-react';
 
 class RecipeCooking extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      instructions: this.props.recipe.instructions
+    }
+  }
+  nextStage(id){
+    this.setState({
+      instructions: this.state.instructions.map(i => i.id === id ? {...i,stage: i.stage===3 ? i.stage=1 : i.stage+=1} : i)
+    })
+  }
   render(){
     const { id, name, ingredients } = this.props.recipe;
+    let instructions = this.state.instructions.filter(i => i.stage === this.props.stage);
     return(
       <Container>
         <Header as='h3'>{name}</Header>
-        {ingredients.map(i => ({
-          <Ingredient ingredient={i}/>
-        }))}
-      <Container/>
+        {instructions.map((i, index) => (
+          <Card
+            className="centered"
+            onClick={() => {
+              this.nextStage(i.id);
+              this.props.update()
+            }}
+            header={i.action + ` the ` + ingredients[index].ingredient}
+          />
+        ))}
+      </Container>
     )
   }
 }
+
+export default RecipeCooking;
